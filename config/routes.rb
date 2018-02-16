cdfaa96482dacd44f8c2751b0a98e2a493142ab0
Gram <g@dented.io>
2018-02-17 01:50:22 +0800

    removed locales and added timestamps

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,13 @@ require 'sidekiq/web'
 Rails.application.routes.draw do
   mount Sidekiq::Web => '/sidekiq-tasks'
 
-  scope "/:locale", locale: /#{I18n.available_locales.join("|")}/ do
-    resources :items, only: [:index, :show] do
-      collection do
-        get :fetch
-      end
+  # scope "/:locale", locale: /#{I18n.available_locales.join("|")}/ do
+  resources :items, only: [:index, :show] do
+    collection do
+      get :fetch
     end
   end
+  # end
 
   root to: 'items#index'
 end
